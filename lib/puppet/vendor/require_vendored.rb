37379399c1e867c513c7effbf59d6e050b74f60b
PrajL <87816065+PrajL@users.noreply.github.com>
2025-07-16 10:33:00 +0530

    Update require_vendored.rb

--- lib/puppet/vendor/require_vendored.rb
+++ lib/puppet/vendor/require_vendored.rb
@@ -1,7 +1,7 @@
 # This adds upfront requirements on vendored code found under lib/vendor/x
 # Add one requirement per vendored package (or a comment if it is loaded on demand).
 
-require 'puppet/vendor/safe_yaml_patches'
+#require 'puppet/vendor/safe_yaml_patches'
 
 # The vendored library 'semantic' is loaded on demand.
 # The vendored library 'rgen' is loaded on demand.
